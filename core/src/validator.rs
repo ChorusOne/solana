6baa5a1a420147d458787f56e4bf1881c5151c5c
Fynn <me@enriquefynn.com>
2022-06-03 14:20:51 -0300

    Initial Prometheus support
    Add Solana node's version
    
    Rename metric
    
    Bugfix: balance should be `gauge`
    
    delete dependabot from our fork
    
    Remove `at` parameter
    
    Change path from `prometheus` to `metrics`
    
    Change `cluster` -> `node`
    
    When referring to our node, we use simply `node` as opposed to
    `cluster`.
    
    Rename metrics
    
    Simplify `Lamports` struct
    
    Change license to Apache 2.0
    
    Add block's timestamp, rename metrics
    
    Remove information about block's transactions
    
    Add label about commitment level, get finalized bank
    
    Also:
      Change help msg for block's timestamp
      Add and rename label
    
    Add metrics for each commitment level
    
    Use `block_commitment_cache`
    
    Instead of propagating `rpc_processor`, use the `block_commitment_cache`
    structure to get different bank confirmation levels
    
    Limit `block_commitment_cache` read lock.
    
    As ruuda pointed out, there is a deadlock if we acquire a read lock for
    both `block_commitment_cache` and `bank_forks`, in ruuda words:
    - We take the read lock on block_commitment_cache.
    - Some other thread takes a write lock on bank_forks.
    - We want to take the read lock on bank_forks, but this blocks because
      the other thread holds a write lock.
    - The other thread wants to take a write lock on block_commitment_cache,
      but that blocks because we hold the read lock.
    - Deadlock!
    
    Instead, we guard the `block_commitment_cache` read lock so we don't
    hold two read locks concurrently. This might lead, as ruuda also
    pointed out, to an inconsistency that's known to Solana and dealt
    already in the code the same way it's dealt in `rpc/src/rpc.rs`.
    
    Correct prometheus type
    
    Should be "counter", not "count"
    
    Save metrics about the validator's vote account
    
    Note, this is a squash of several commits to make the patch easier to
    rebase:
    
      Get last vote
    
      Clone Lamports struct
    
      Add label to vote balance
    
      Rename labels
    
      Add parameter to observe vote accounts
    
        Down the rabbit hole from the arguments, propagating it to the
        prometheus part, we add a flag so we can track multiple vote accounts.
    
      Add vote metrics
    
        Modify how metrics are written to account for when there's nothing to
        report. Add metrics about votes: last voted and vote balance
    
      Naming changes, add vote credits information
    
        Change variable and function names, and add information about vote
        credits
    
      Write vote account metrics
    
      Slight change on how metrics are recorded
    
      Move field to avoid conflicts with other patches
    
      Rename prometheus metrics help
    
      Refer as `validator` metrics
    
      Rename variables
    
      Bugfix: Correct initialization
    
      Add `vote_accounts_to_monitor` to `replica_node`
    
    Fix identity pubkeyt for each validator to monitor
    
    Change metric name
    
    Co-authored-by: Ruud van Asseldonk <dev@veniogames.com>
    
    Add active stake per monitored validator
    
    Update metric name
    
    Co-authored-by: Ruud van Asseldonk <dev@veniogames.com>
    
    Expose epoch schedule metrics in Prometheus
    
    With the epoch start slots and the number of slots in the epoch (and the
    current slot, which we already had), we can infer/estimate:
    
     * Epoch progress percentage
     * Slots left until the next epoch
     * Time left until the next epoch (from slot height increase)
    
    These are useful metrics to have about the network.
    
    Add validator name label to metrics
    
    Get validator info on startup.
    
    Format code
    
    Minor review changes
    
    Expose latest full and incremental snapshots metrics
    
    Change metrics to gauge, adjust names
    
    Also squashed commits:
    
      Remove hash label
      Fix compile error in snapshot metrics
    
    Ignore changes in Cargo.lock
    
    Add flag to enable prometheus metrics
    
    Fix rpc service tests
    
    Add missing with_suffix method to Metric
    
    Prometheus: Initialize identity map in separate thread
    
    cargo fmt
    
    Prometheus: Add logs around loading accounts
    
    Load identity accounts from file
    
    Expose metrics without validator info
    
    Add support for monitoring arbitrary accounts' balances
    
    Use a config file for specifying monitoring info

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -240,6 +240,8 @@ pub struct ValidatorConfig {
     pub repair_validators: Option<HashSet<Pubkey>>, // None = repair from all
     pub repair_whitelist: Arc<RwLock<HashSet<Pubkey>>>, // Empty = repair with all
     pub gossip_validators: Option<HashSet<Pubkey>>, // None = gossip with all
+    pub monitor_accounts_config_path: Option<PathBuf>,
+    pub default_vote_account_to_monitor: Option<Pubkey>,
     pub accounts_hash_interval_slots: u64,
     pub max_genesis_archive_unpacked_size: u64,
     pub wal_recovery_mode: Option<BlockstoreRecoveryMode>,
@@ -314,7 +316,9 @@ impl Default for ValidatorConfig {
             repair_validators: None,
             repair_whitelist: Arc::new(RwLock::new(HashSet::default())),
             gossip_validators: None,
-            accounts_hash_interval_slots: u64::MAX,
+            monitor_accounts_config_path: None,
+            default_vote_account_to_monitor: None,
+            accounts_hash_interval_slots: std::u64::MAX,
             max_genesis_archive_unpacked_size: MAX_GENESIS_ARCHIVE_UNPACKED_SIZE,
             wal_recovery_mode: None,
             run_verification: true,
@@ -1009,6 +1013,7 @@ impl Validator {
 
         let rpc_override_health_check =
             Arc::new(AtomicBool::new(config.rpc_config.disable_health_check));
+        let rpc_enable_prometheus_metrics = config.rpc_config.rpc_enable_prometheus_metrics;
         let (
             json_rpc_service,
             pubsub_service,
@@ -1049,6 +1054,7 @@ impl Validator {
                 config.validator_exit.clone(),
                 exit.clone(),
                 rpc_override_health_check.clone(),
+                rpc_enable_prometheus_metrics,
                 startup_verification_complete,
                 optimistically_confirmed_bank.clone(),
                 config.send_transaction_service_config.clone(),
@@ -1058,6 +1064,8 @@ impl Validator {
                 max_complete_transaction_status_slot,
                 max_complete_rewards_slot,
                 prioritization_fee_cache.clone(),
+                config.monitor_accounts_config_path.clone(),
+                config.default_vote_account_to_monitor,
             )
             .map_err(ValidatorError::Other)?;
 
