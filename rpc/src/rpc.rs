6111d5668c065dcc8109465fa95ed6e6beea4a8f
Fynn <me@enriquefynn.com>
2022-06-16 14:10:00 -0300

    Use `block_commitment_cache`
    Instead of propagating `rpc_processor`, use the `block_commitment_cache`
    structure to get different bank confirmation levels

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -186,7 +186,7 @@ impl Metadata for JsonRpcRequestProcessor {}
 
 impl JsonRpcRequestProcessor {
     #[allow(deprecated)]
-    pub fn bank(&self, commitment: Option<CommitmentConfig>) -> Arc<Bank> {
+    fn bank(&self, commitment: Option<CommitmentConfig>) -> Arc<Bank> {
         debug!("RPC commitment_config: {:?}", commitment);
 
         let commitment = commitment.unwrap_or_default();
